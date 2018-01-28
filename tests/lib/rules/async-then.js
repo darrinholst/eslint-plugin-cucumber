74ebb2248fa519eeb893bb48a9aac826d5ac847f
Darrin Holst <darrinholst@gmail.com>
2018-01-28 13:45:33 -0600

    add support for cucumber 2+ step definitions

--- tests/lib/rules/async-then.js
+++ tests/lib/rules/async-then.js
@@ -1,23 +1,36 @@
-'use strict';
-
-var rule = require('../../../lib/rules/async-then');
-var RuleTester = require('eslint').RuleTester;
+const rule = require('../../../lib/rules/async-then');
+const RuleTester = require('eslint').RuleTester;
 
 new RuleTester().run('async-then', rule, {
   valid: [
     'this.Then(/step/, function () {return "anything";})',
+    'Then(/step/, function () {return "anything";})',
     'this.Then(/step/, function (done) {})',
+    'Then(/step/, function (done) {})',
     'this.Then(/step/, function (next) {})',
+    'Then(/step/, function (next) {})',
     'this.Given(/step/, function () {})',
-    'this.When(/step/, function () {})'
+    'Given(/step/, function () {})',
+    'this.When(/step/, function () {})',
+    'When(/step/, function () {})'
   ],
 
   invalid: [
     {
       code: 'this.Then(/step/, function () {})',
-      errors: [{
-        message: 'Then step didn\'t return a promise or have a callback.',
-      }]
+      errors: [
+        {
+          message: "Then step didn't return a promise or have a callback."
+        }
+      ]
+    },
+    {
+      code: 'Then(/step/, function () {})',
+      errors: [
+        {
+          message: "Then step didn't return a promise or have a callback."
+        }
+      ]
     }
   ]
 });
--- tests/lib/rules/no-restricted-tags.js
+++ tests/lib/rules/no-restricted-tags.js
@@ -1,19 +1,18 @@
-'use strict';
+const plugin = require('../../../lib/index');
+const rule = require('../../../lib/rules/no-restricted-tags');
+const RuleTester = require('eslint').RuleTester;
 
-var plugin = require('../../../lib/index');
-var rule = require('../../../lib/rules/no-restricted-tags');
-var RuleTester = require('eslint').RuleTester;
-
-new RuleTester().run("no-restricted-tags", rule, {
-  valid: [
-    { code: preprocess("@wip"), options: ["foo"]}
-  ],
-  invalid: [{
-    code: preprocess("@wip"), options: ["foo", "wip", "bar"],
-    errors: [{ message: "'@wip' is restricted from being used."}]
-  }]
+new RuleTester().run('no-restricted-tags', rule, {
+  valid: [{code: preprocess('@wip'), options: ['foo']}],
+  invalid: [
+    {
+      code: preprocess('@wip'),
+      options: ['foo', 'wip', 'bar'],
+      errors: [{message: "'@wip' is restricted from being used."}]
+    }
+  ]
 });
 
 function preprocess(code) {
-  return plugin.processors['.feature'].preprocess(code).join("\n");
+  return plugin.processors['.feature'].preprocess(code).join('\n');
 }
