57dd90549a2b90deaa28d7f86aec84c4c153f169
Colin Wren <gimpneek@gmail.com>
2018-08-12 20:04:08 +0100

    - Adding rule to disallow arrow functions on cucumber step definitions

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 node tests/lib/rules/async-then.js && \
-node tests/lib/rules/no-restricted-tags.js
+node tests/lib/rules/no-restricted-tags.js && \
+node tests/lib/rules/no-cucumber-arrows.js
